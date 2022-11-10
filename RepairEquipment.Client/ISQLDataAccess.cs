dd6e16d95badadcc3fd021b3bbc8aa39fad84bb5
cimpal55 <cimpals71@gmail.com>
2022-11-10 11:22:42 +0200

    Recommit

--- RepairEquipment.Client/ISQLDataAccess.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RepairEquipment.Client
-{
-    public interface ISQLDataAccess
-    {
-        Task<List<T>> LoadData<T, U>(string sql, U parameters, string connectionString);
-        Task SaveData<T>(string sql, T parameters, string connectionString);
-    }
-}
