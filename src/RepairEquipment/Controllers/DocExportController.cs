df81cc82ec4bb0cdb2a6902625ab099a37689635
cimpal55 <cimpals71@gmail.com>
2022-12-27 21:03:41 +0200

    Added doc export controller, fixed search on each page

---
+++ src/RepairEquipment/Controllers/DocExportController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RepairEquipment.Client.Services.Interfaces;
+using RepairEquipment.Shared.Models.Export;
+
+namespace RepairEquipment.Controllers
+{
+    [Route("api/doc-export")]
+    [ApiController]
+    public sealed class DocExportController : ControllerBase
+    {
+        private readonly IDocExportService _docExportService;
+        public DocExportController(IDocExportService docExportService)
+        {
+            _docExportService = docExportService;
+        }
+
+        [HttpGet("file")]
+        public async Task<FileStreamResult> ExportFile([FromQuery(Name = "fmt")] string format, [FromQuery(Name = "doctype")] string docType, [FromQuery(Name = "search")] string? search = null, CancellationToken ct = default)
+        {
+            var req = new DocExportFileRequest
+            {
+                Format = format,
+                DocType = docType
+            };
+
+            var res = await _docExportService.ExportPdfAsync(req, search, ct)
+                .ConfigureAwait(false);
+
+            return File(res.FileStream, res.ContentType, res.FileName);
+        }
+
+    }
+}
